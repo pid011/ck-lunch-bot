fcc0fc57f877783420256246adf55821b39870eb
pid011 <pid011@kakao.com>
2022-02-28 08:32:00 +0900

    Fix SSL connection bug

--- src/CKLunchBot.Core/Requester/WebClient.cs
+++ src/CKLunchBot.Core/Requester/WebClient.cs
@@ -5,5 +5,9 @@ namespace CKLunchBot.Core.Requester;
 internal static class WebClient
 {
     // https://docs.microsoft.com/en-us/dotnet/api/system.net.http.httpclient#remarks
-    public static readonly HttpClient Client = new();
+    public static readonly HttpClient Client = new(new HttpClientHandler
+    {
+        // https://stackoverflow.com/questions/52939211/the-ssl-connection-could-not-be-established
+        ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
+    });
 }
