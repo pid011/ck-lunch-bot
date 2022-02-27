7d28595782132c0c09950b6255ab9fda8def324e
pid011 <pid011@kakao.com>
2022-02-27 14:05:40 +0900

    aaaa

--- src/CKLunchBot.Core/Drawing/AssetPath.cs
+++ src/CKLunchBot.Core/Drawing/AssetPath.cs
@@ -5,9 +5,9 @@ namespace CKLunchBot.Core.Drawing;
 
 internal static class AssetPath
 {
-    private static readonly string s_assetsPath = Path.Combine(Environment.CurrentDirectory, "Assets");
-    private static readonly string s_fontsPath = Path.Combine(s_assetsPath, "Fonts");
-    private static readonly string s_imagesPath = Path.Combine(s_assetsPath, "Images");
+    private static readonly string s_assetsPath = Path.Combine(Environment.CurrentDirectory, "assets");
+    private static readonly string s_fontsPath = Path.Combine(s_assetsPath, "fonts");
+    private static readonly string s_imagesPath = Path.Combine(s_assetsPath, "images");
 
     public static readonly string BoldFont = Path.Combine(s_fontsPath, "MaruBuri-Bold.ttf");
     public static readonly string RegularFont = Path.Combine(s_fontsPath, "MaruBuri-Regular.ttf");
