1acf7e9346a27f07e9ae1b3b00b578349a330a53
pid011 <pid011@naver.com>
2024-03-13 02:51:24 +0900

    Reduce length of date format text

--- src/CKLunchBot.Core.Tests/MenuTest.cs
+++ src/CKLunchBot.Core.Tests/MenuTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CKLunchBot.Core.Tests;
@@ -5,4 +6,11 @@ namespace CKLunchBot.Core.Tests;
 [TestClass]
 public class MenuTest
 {
+    [TestMethod]
+    public void DateFormatTextTest()
+    {
+        var date = new DateOnly(2022, 1, 1);
+        var formatted = date.GetFormattedKoreanString();
+        Assert.AreEqual(formatted, "2022.1.1 (토)");
+    }
 }
